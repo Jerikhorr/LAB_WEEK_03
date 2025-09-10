b3a2ab5147753a50ad25c537900642bf2798c2af
Jerikhorr <133670400+Jerikhorr@users.noreply.github.com>
2025-09-10 20:20:09 +0700

    Commit No. 3 – use dynamic fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,13 +36,18 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // Navigation (dynamic fragments)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
+
